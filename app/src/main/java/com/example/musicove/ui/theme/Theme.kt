d65f3e565e6425a5af8a2ac3c99724313fe39c96
Tanuj Khanna <tanujkhanna72@gmail.com>
2024-07-09 19:15:37 +0530

    Setting up theme and permissions

--- app/src/main/java/com/example/musicove/ui/theme/Theme.kt
+++ app/src/main/java/com/example/musicove/ui/theme/Theme.kt
@@ -1,69 +1,67 @@
 package com.example.musicove.ui.theme
 
-import android.app.Activity
-import android.os.Build
+import android.annotation.SuppressLint
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
+import com.dev.headphoneplayer.ui.theme.BackGroundColor
+import com.dev.headphoneplayer.ui.theme.Black1
+import com.dev.headphoneplayer.ui.theme.Black2
+import com.dev.headphoneplayer.ui.theme.Black3
+import com.dev.headphoneplayer.ui.theme.Gray200
+import com.dev.headphoneplayer.ui.theme.Gray300
+import com.dev.headphoneplayer.ui.theme.RedErrorDark
+import com.dev.headphoneplayer.ui.theme.RedErrorLight
+import com.dev.headphoneplayer.ui.theme.SurfaceColor
+import com.dev.headphoneplayer.ui.theme.Teal200
+import com.dev.headphoneplayer.ui.theme.TealA700
+import com.dev.headphoneplayer.ui.theme.White
+import com.dev.headphoneplayer.ui.theme.onBackGround
+import com.dev.headphoneplayer.ui.theme.onSurface
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+@SuppressLint("ConflictingOnColor")
+private val DarkColorPalette = darkColorScheme(
+    primary = Teal200,
+    onPrimary = White,
+    primaryContainer = TealA700,
+    secondary = Teal200,
+    error = RedErrorLight,
+    background = Black2,
+    onBackground = Gray300,
+    surface = Black3,
+    onSurface = Gray200,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+@SuppressLint("ConflictingOnColor")
+private val LightColorPalette = lightColorScheme(
+    primary = Teal200,
+    onPrimary = White,
+    primaryContainer = TealA700,
+    secondary = Teal200,
+    onSecondary = Black1,
+    error = RedErrorDark,
+    onError = RedErrorLight,
+    background = BackGroundColor,
+    onBackground = onBackGround,
+    surface = SurfaceColor,
+    onSurface = onSurface,
 )
 
 @Composable
 fun MusiCoveTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colors,
         typography = Typography,
         content = content
     )
--- app/src/main/java/com/example/musicove/ui/theme/Type.kt
+++ app/src/main/java/com/example/musicove/ui/theme/Type.kt
@@ -1,34 +1,85 @@
 package com.example.musicove.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.musicove.R
 
 // Set of Material typography styles to start with
+private val nunito = FontFamily(
+    Font(R.font.nunito_regular, FontWeight.W400),
+    Font(R.font.nunito_medium, FontWeight.W500),
+    Font(R.font.nunito_bold, FontWeight.W600),
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    headlineLarge = TextStyle(
+        fontFamily = nunito,
+        fontWeight = FontWeight.W500,
+        fontSize = 30.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    headlineMedium = TextStyle(
+        fontFamily = nunito,
+        fontWeight = FontWeight.W500,
+        fontSize = 24.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = nunito,
+        fontWeight = FontWeight.W500,
+        fontSize = 20.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = nunito,
+        fontWeight = FontWeight.W400,
+        fontSize = 18.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = nunito,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = nunito,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = nunito,
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = nunito,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = nunito,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = nunito,
+        fontSize = 14.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = nunito,
+        fontWeight = FontWeight.W400,
+        fontSize = 15.sp,
+        color = Color.White
+    ),
+    displayMedium = TextStyle(
+        fontFamily = nunito,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = nunito,
+        fontWeight = FontWeight.W400,
+        fontSize = 10.sp
     )
-    */
 )
